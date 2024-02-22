078842a704a6cb1787a8646f427a4fd61402be92
Mohamad44p <mohamad44p@yahoo.com>
2024-02-22 21:31:30 +0200

    Fixed

---
+++ src/components/About/About.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function About() {
+
+  let [count,setCount] = useState(0);
+  const changeCount = ()=>{
+    count = count + 1;
+    setCount(count);
+  };
+  return (
+    <div>
+      <button onClick={changeCount}>count : {count}</button>
+    </div>
+  )
+}
+
+export default About
